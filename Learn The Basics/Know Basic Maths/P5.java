2e23d0e080e808668dd2d6ce78595aa2e017fd60
SsmayankGupta <mgmayank2240@gmail.com>
2026-08-25 07:06:49 +0530

    Killer Man

--- Learn The Basics/Know Basic Maths/P5.java
+++ Learn The Basics/Know Basic Maths/P5.java
@@ -1,16 +1,17 @@
 public class P5 {
     public static void main(){
-        int num=198,cnum=num;int sum=0;
+        int num=1,cnum=num;int sum=0;
         int digits=0;
-        for(int i=1;i<=num;i++){
+        for(int i=1;i<=cnum;i++){
             digits=num%10;
             sum+=Math.pow(digits,3);
             num/=10;
+            if(num==0)break;
         }
         if(num==sum)
         System.out.println("Armstrong number");
         else
         System.out.println("Not Armstrong number");
-        
+
     }
 }
